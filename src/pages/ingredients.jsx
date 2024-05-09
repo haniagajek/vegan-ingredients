f7dff2047ddea76061120c2d32d145a31b5581f4
haniagajek <97366087+haniagajek@users.noreply.github.com>
2024-05-09 21:53:26 +0200

    add table update

--- src/pages/ingredients.jsx
+++ src/pages/ingredients.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
 
+// trzeba zrobic zmienna ktora przyjmuje numer przedzialu w tabeli tak zeby byl np 1 i 1 i na pierwszym bedzie func(click) a na drugim bedzie wysuwanie
 /**
  * Renders the Home component.
  * @remarks
@@ -9,30 +10,61 @@ import React from "react";
  */
 export function Ingredients() {
   return (
-    <div className="max-w-max mx-auto ">
-      <table className="text-tod border-separate border-spacing-x-20 border-spacing-y-8 border mt-24 text-center">
-        <thead>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg ">
+      <table class="w-full text-sm text-left rtl:text-right text-gray-500 ">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-200 ">
           <tr>
-            <th className="bg-blo px-14 hover:bg-blt">Meal</th>
-            <th className="bg-blo px-14 hover:bg-blt">Calories</th>
-            <th className="bg-blo px-14 hover:bg-blt">Level</th>
+            <th scope="col" class="px-6 py-3">
+              Meal name
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Calories
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Level
+            </th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Sesame Noodles</td>
-            <td>1000kcal</td>
-            <td>Easy</td>
+          <tr class="odd:bg-white  even:bg-gray-200 ">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+            >
+              Pumpkin risotto
+            </th>
+            <td class="px-6 py-4">1000 kcal</td>
+            <td class="px-6 py-4">Medium</td>
           </tr>
-          <tr>
-            <td>Pumpkin Risotto</td>
-            <td>1000kcal</td>
-            <td>Medium</td>
+          <tr class="odd:bg-white  even:bg-gray-200 border-b ">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap e"
+            >
+              info
+            </th>
+            <td class="px-6 py-4">info</td>
+            <td class="px-6 py-4">info</td>
           </tr>
-          <tr>
-            <td>Green Tahini Dip</td>
-            <td>1000kcal</td>
-            <td>Easy</td>
+          <tr class="odd:bg-white  even:bg-gray-200 ">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+            >
+              Butter masala
+            </th>
+            <td class="px-6 py-4">1000 kcal</td>
+            <td class="px-6 py-4">Easy</td>
+          </tr>
+          <tr class="odd:bg-white  even:bg-gray-200 ">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+            >
+              info
+            </th>
+            <td class="px-6 py-4">info</td>
+            <td class="px-6 py-4">info</td>
           </tr>
         </tbody>
       </table>
